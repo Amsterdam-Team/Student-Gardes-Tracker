27a0722202239bf146bef47dc021448f75293631
raouf-kamel <raoufkamel2013@gmail.com>
2025-04-08 23:02:42 +0300

    add check

--- src/main/kotlin/feature/monthly_summary/monthly_summary_test_case.kt
+++ src/main/kotlin/feature/monthly_summary/monthly_summary_test_case.kt
@@ -3,3 +3,10 @@ package feature.monthly_summary
 fun main(args: Array<String>) {
     println("monthly_summary_test_case!")
 }
+fun check(name: String, result: Boolean, correctResult: Boolean) {
+    if (result == correctResult) {
+        println("Success - $name")
+    } else {
+        println("Failed - $name")
+    }
+}
